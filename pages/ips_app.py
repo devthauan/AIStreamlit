f495cd849211c6feae1add5d0b13278c4529b625
thauansilva05 <thauan.cruz@radixeng.com>
2024-10-08 14:45:27 -0300

    filters on ips

--- pages/ips_app.py
+++ pages/ips_app.py
@@ -12,8 +12,9 @@ st.set_page_config(
     page_title="Incident Probability Score",
     )
 # MÉTRICAS DA ANOMALIA ################
-def anomaly_metrics(df, threshold=50, time_window='20min'):
-    df_last_minutes = df.last(time_window)
+def anomaly_metrics(df, threshold=50, start_time=None, end_time=None):
+
+    df_last_minutes = df[(df.index>=start_time) & (df.index<=end_time)]
     anomalies = df_last_minutes['anomaly_probability'] >= threshold
     total_anomalies = anomalies.sum()
     anomaly_percentage = (total_anomalies / len(df_last_minutes)) * 100
@@ -22,11 +23,11 @@ def anomaly_metrics(df, threshold=50, time_window='20min'):
     max_anomaly = df_last_minutes['anomaly_probability'].max()
 
     stats = {
-        'total_anomalies': total_anomalies,
+        'total_anomalies': int(total_anomalies),
         'anomaly_percentage': round(anomaly_percentage, 2),
         'avg_anomaly_score': round(anomaly_average_score, 2) if not pd.isna(anomaly_average_score) else np.nan,
         'anomaly_std': round(anomaly_std, 2) if not pd.isna(anomaly_std) else np.nan,
-        'max_anomaly': round(max_anomaly, 2)
+        'max_anomaly': round(max_anomaly,2)
     }
     return stats
 
@@ -67,24 +68,53 @@ df_outputs = get_data_from_cognite("dev")
 # LEITURA DO ARQUIVO ################
 df_tested_data = df_outputs
 
-if not pd.api.types.is_datetime64_any_dtype(df_tested_data.index):
-    df_tested_data.index = pd.to_datetime(df_tested_data.index)
+# if not pd.api.types.is_datetime64_any_dtype(df_tested_data.index)
+df_tested_data.index = pd.to_datetime(df_tested_data.index)
+
+# DATETIME INTERVAL FILTER #################
+st.sidebar.header("Date Interval")
+
+tzinfo = pytz.utc
+date_now = datetime.now(tzinfo)
+
+# Selecionar o intervalo de datas
+start_date = st.sidebar.date_input("Start Date", value=date_now - timedelta(days = 7))
+
+end_date = st.sidebar.date_input("End Date", value=date_now)
+
+# Selecionar a hora e minuto para o tempo final
+end_time = st.sidebar.time_input("Select End Time (related to end date)", value=pd.to_datetime("2022-08-29 23:59:00").time())
+
+# Criar o datetime final combinando a data final com a hora e minuto selecionados
+end_datetime = pd.to_datetime(f"{end_date} {end_time}")
+
+# Definir um intervalo de tempo que o usuário deseja visualizar (em minutos)
+time_window_minutes = st.sidebar.slider("Select Time Window (in minutes)", 0, 1440, 60)  # Máximo de 24 horas
+
+# Calcular o start_datetime baseado no intervalo de tempo escolhido
+start_datetime = end_datetime - pd.Timedelta(minutes=time_window_minutes)
+
+# Filtrando o DataFrame com base no intervalo de datetime selecionado
+filtered_data = df_tested_data[start_datetime:end_datetime]
+
+# filtered_data = df_tested_data.loc[start_date:end_date]
+if len(filtered_data)==0:
+    st.warning("there are no data on this time period!")
+
+# Calcular e exibir as métricas de anomalia para o intervalo selecionado
+anomaly_stats = anomaly_metrics(df_tested_data, start_time=start_datetime, end_time=end_datetime)
+
+# Exibir título dinâmico com o intervalo de tempo escolhido
+# st.markdown(f'<span style="font-size: 20px;">**Anomaly Metrics From the dates {start_datetime} to {end_datetime}**</span>', unsafe_allow_html=True)
+st.markdown(f'<span style="font-size: 20px;">**Anomaly Metrics From the date interval: {start_date} to {end_datetime}**</span>', unsafe_allow_html=True)
+
 
 # CAIXA NA BARRA LATERAL ################
 st.sidebar.header("Variables")
 columns = ['Pressure', 'Speed', 'Flow']
 selected_column = st.sidebar.selectbox("Choose the time series:", columns)
 
-# CALCULO DAS METRICAS ################
-anomaly_stats = anomaly_metrics(df_tested_data)
-
-# CALCULO DO DESVIO PADRAO PLOT(1,1) ################
-# df_tested_data['rolling_std'] = df_tested_data['anomaly_probability'].rolling('60min').std()
-
-# DISPLAY BIGNUMBERS ################
-# st.markdown("### Anomaly Metrics Last 20 minutes")
-st.markdown('<span style="font-size: 18px;">**Anomaly Metrics Last 20 minutes**</span>', unsafe_allow_html=True)
-
+# Exibir métricas
 col1, col2, col3, col4, col5 = st.columns(5)
 
 col1.metric("Total Anomalies", anomaly_stats['total_anomalies'])
@@ -93,20 +123,25 @@ col3.metric("Avg Anomaly Score", f"{anomaly_stats['avg_anomaly_score']}%")
 col4.metric("Max Anomaly", f"{anomaly_stats['max_anomaly']}%")
 col5.metric("Std Anomaly", anomaly_stats['anomaly_std'])
 
+
+df_date_interval = df_tested_data[start_date:end_date]
+
 # PLOT SÉRIE TEMPORAL X ANOMALIA (1,1) ################
 fig = go.Figure()
 
 # Adicionando a série temporal escolhida
-fig.add_trace(go.Scatter(x=df_tested_data.index, y=df_tested_data[selected_column],
+fig.add_trace(go.Scatter(x=df_date_interval.index, y=df_date_interval[selected_column],
                          mode='lines', name=selected_column, line=dict(color='blue')))
 
-fig.add_trace(go.Scatter(x=df_tested_data.index, y=df_tested_data['anomaly_probability'],
+fig.add_trace(go.Scatter(x=df_date_interval.index, y=df_date_interval['anomaly_probability'],
                          mode='lines', name='anomaly(%)',
                          line=dict(color='orange', dash='dash'),
                          yaxis='y2'))
+
+
 # Configurações do layout com y-axis secundário
 fig.update_layout(
-    title=f'{selected_column} vs Anomaly Probability',
+    title=f'{selected_column} vs Anomaly Probability {start_date} to {end_date}',
     xaxis_title='Date',
     yaxis_title=selected_column,
     yaxis2=dict(
@@ -118,25 +153,24 @@ fig.update_layout(
 )
 
 # Calcular o rolling_std com uma janela de 5 minutos para os últimos 100 minutos
-df_last_100_min = df_tested_data.last('100min')
-# df_last_100_min['rolling_std_5min'] = df_last_100_min['anomaly_probability'].rolling('5min').std()
+df_end_data = df_tested_data[start_datetime:end_datetime]
 
 # PLOT SÉRIE TEMPORAL X ANOMALIA (1,2) ################
-fig_last_100 = go.Figure()
+fig_last_x_min = go.Figure()
 
 # Adicionando a série temporal escolhida
-fig_last_100.add_trace(go.Scatter(x=df_last_100_min.index, y=df_last_100_min[selected_column],
+fig_last_x_min.add_trace(go.Scatter(x=df_end_data.index, y=df_end_data[selected_column],
                                     mode='lines', name=selected_column, line=dict(color='blue')))
 
 # Adicionando o desvio padrão no eixo y secundário (cor laranja)
-fig_last_100.add_trace(go.Scatter(x=df_last_100_min.index, y=df_last_100_min['anomaly_probability'],
+fig_last_x_min.add_trace(go.Scatter(x=df_end_data.index, y=df_end_data['anomaly_probability'],
                                     mode='lines', name='anomaly(%)',
                                     line=dict(color='orange', dash='dash'),
                                     yaxis='y2'))
 
 # Configurações do layout do gráfico de últimos 100 minutos
-fig_last_100.update_layout(
-    title=f'{selected_column} (Last 100 min) vs Anomaly Probability',
+fig_last_x_min.update_layout(
+    title=f'{selected_column} (last {time_window_minutes} minutes) vs Anomaly Probability',
     xaxis_title='Date',
     yaxis_title=selected_column,
     yaxis2=dict(
@@ -148,9 +182,9 @@ fig_last_100.update_layout(
 )
 
 # PLOT HISTOGRAMA COM BLOXPOT (2,1) ################
-fig_hist_box = px.histogram(df_tested_data, x='anomaly_probability', nbins=30, marginal="box", color_discrete_sequence=['green'])
+fig_hist_box = px.histogram(df_date_interval, x='anomaly_probability', nbins=30, marginal="box", color_discrete_sequence=['green'])
 fig_hist_box.update_layout(
-    title="Anomaly Probability Distribution",
+    title=f"Anomaly Probability Distribution {start_date} to {end_date}",
     xaxis_title="Anomaly Probability",
     yaxis_title="Count",
     showlegend=False
@@ -159,14 +193,14 @@ fig_hist_box.update_layout(
 # PLOT BARRAS EMPILHADAS (2,2) ################
 
 # Classificação de anomalia
-df_tested_data['anomaly'] = (df_tested_data['anomaly_probability'] >= 50).astype(int)
+df_date_interval['anomaly'] = (df_date_interval['anomaly_probability'] >= 50).astype(int)
 
 # Adicionar colunas para o dia da semana
-df_tested_data['weekday'] = df_tested_data.index.weekday  # 0 = Monday, 6 = Sunday
+df_date_interval['weekday'] = df_date_interval.index.weekday  # 0 = Monday, 6 = Sunday
 
 # Contar a quantidade de anomalias (1) e inliers (0) por dia da semana
-anom_count = df_tested_data[df_tested_data['anomaly'] == 1].groupby('weekday').size()
-non_anom_count = df_tested_data[df_tested_data['anomaly'] == 0].groupby('weekday').size()
+anom_count = df_date_interval[df_date_interval['anomaly'] == 1].groupby('weekday').size()
+non_anom_count = df_date_interval[df_date_interval['anomaly'] == 0].groupby('weekday').size()
 
 # Criar um DataFrame para organizar os dados
 df_bars = pd.DataFrame({
@@ -183,7 +217,7 @@ df_bars['Inliers_percent'] = (df_bars['Inliers']/df_bars['Total'])*100
 
 # Definir os rótulos dos dias da semana
 days_labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-# df_bars = df_bars.reindex(range(7), fill_value=0)
+
 # Gráfico de barras empilhadas
 fig_bars = go.Figure()
 fig_bars.add_trace(go.Bar(x=days_labels, y=df_bars['Inliers_percent'], name='Inliers', marker_color='blue'))
@@ -191,7 +225,7 @@ fig_bars.add_trace(go.Bar(x=days_labels, y=df_bars['Anomalies_percent'], name='A
 
 # Configurações do layout do gráfico de barras
 fig_bars.update_layout(
-    title='Anomaly and Inlier Percentages by Day of the Week',
+    title=f'Anomaly and Inlier Percentages by Day of the Week {start_date} to {end_date}',
     xaxis_title='Day of the Week',
     yaxis_title='Percentage',
     barmode='stack',
@@ -207,7 +241,7 @@ with col1:
     st.plotly_chart(fig, use_container_width=True)
 
 with col2:
-    st.plotly_chart(fig_last_100, use_container_width=True)
+    st.plotly_chart(fig_last_x_min, use_container_width=True)
 
 # Segunda linha com mais dois gráficos lado a lado
 # st.markdown("## Anomaly Probability and Day of the Week Analysis")
